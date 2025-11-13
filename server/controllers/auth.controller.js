bca9a2ab15c176a70c5ec21cd50c45b2774c5238
tabyasarao <tsarao1@my.centennialcollege.ca>
2025-11-13 09:21:33 -0500

    changed "403" to 403

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -45,7 +45,7 @@ const requireSignin = expressjwt({
 const hasAuthorization = (req, res, next) => {
   const authorized = req.profile && req.auth && req.profile._id == req.auth._id;
   if (!authorized) {
-    return res.status("403").json({
+    return res.status(403).json({
       error: "User is not authorized",
     });
   }
