de967c51dbe1b4e640b452b23110d3aa9cf2eb47
tabyasarao <tsarao1@my.centennialcollege.ca>
2025-11-14 12:46:53 -0500

    Refactor main entry to use createRoot and StrictMode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
-import './index.css' // optional, can create an empty file if you don't have CSS yet
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
