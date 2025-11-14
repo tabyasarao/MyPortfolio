147954105926d53ba8668baf13ffec7c4266e0f0
tabyasarao <tsarao1@my.centennialcollege.ca>
2025-11-14 12:49:47 -0500

    update root render to pass props and clean up imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
+import react from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    (<App tab="home" />
+    )
+  </StrictMode>
+);
